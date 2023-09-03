f3d519fb5aed035af6e901afa0f93860dda87b1c
Aina_Kostina <aina.kostina@1clickgames.com>
2023-09-03 14:58:47 +0300

    Added form with inputs

--- src/App.js
+++ src/App.js
@@ -23,9 +23,44 @@ function App() {
   const [members, setMembers] = useState(membersData);
 
   return (
-    <div className="App">
-      <table className="App__table">
-        <thead className="App__thead">
+    <div className='App'>
+      <form className='App__form'>
+        <input
+          type='text'
+          name='fullName'
+          required='required'
+          placeholder='Name'
+          onFocus={(e) => e.target.placeholder = ''}
+          onBlur={(e) => e.target.placeholder = 'Name'}
+        />
+        <input
+          type='text'
+          name='fullSurName'
+          required='required'
+          placeholder='Surname'
+          onFocus={(e) => e.target.placeholder = ''}
+          onBlur={(e) => e.target.placeholder = 'Surname'}
+        />
+        <input
+          type='number'
+          name='age'
+          required='required'
+          placeholder='Age'
+          onFocus={(e) => e.target.placeholder = ''}
+          onBlur={(e) => e.target.placeholder = 'Age'}
+        />
+        <input
+          type='text'
+          name='city'
+          required='required'
+          placeholder='City'
+          onFocus={(e) => e.target.placeholder = ''}
+          onBlur={(e) => e.target.placeholder = 'City'}
+        />
+        <button type='submit' className='App__form__button'>Add</button>
+      </form>
+      <table className='App__table'>
+        <thead className='App__thead'>
           <tr>
             <th>Name</th>
             <th>Surname</th>
@@ -33,7 +68,7 @@ function App() {
             <th>City</th>
           </tr>
         </thead>
-        <tbody className="App__tbody">
+        <tbody className='App__tbody'>
           {members.map(({name, surname, age, city}) => (
             <tr>
               <td>{name}</td>
