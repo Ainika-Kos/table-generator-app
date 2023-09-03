e20ff54c614cdb5e1a71115f6c7b73f55009bcc0
Aina_Kostina <aina.kostina@1clickgames.com>
2023-09-03 14:15:58 +0300

    table with static data without form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import './App.scss';
+import './App.sass';
+import { useState } from 'react';
+
+const membersData = [
+  {
+    'id': 1,
+    'name': 'Nanana',
+    'surname': 'Lalala',
+    'age': 28,
+    'city': 'Carnikava'
+  },
+  {
+    'id': 2,
+    'name': 'Hello',
+    'surname': 'Sunshine',
+    'age': 36,
+    'city': 'Gauja'
+  }
+];
 
 function App() {
+
+  const [members, setMembers] = useState(membersData);
+
   return (
     <div className="App">
-      <p>
-        Test
-      </p>
+      <table className="App__table">
+        <thead className="App__thead">
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Age</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody className="App__tbody">
+          {members.map(({name, surname, age, city}) => (
+            <tr>
+              <td>{name}</td>
+              <td>{surname}</td>
+              <td>{age}</td>
+              <td>{city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
