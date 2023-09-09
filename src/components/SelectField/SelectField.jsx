6460211e437b5176e29e25dbb6f378d08f971f26
Aina_Kostina <aina.kostina@1clickgames.com>
2023-09-10 00:03:54 +0300

    Custom select field

--- src/components/SelectField/SelectField.jsx
+++ src/components/SelectField/SelectField.jsx
@@ -1,23 +1,49 @@
 import cities from '../../data/cities';
+import './SelectField.sass';
+import { useState, useEffect } from 'react';
 
-const SelectField = ({name, onChange, value, isControlledInput}) => {
+const SelectField = ({ name, onChange, value, isControlledInput }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(value);
+
+  useEffect(() => {
+    setSelectedValue(value);
+  }, [value]);
+
+  const handleSelect = (option) => {
+    if (!isControlledInput) {
+      setSelectedValue(option);
+    }
+    const event = {
+      target: {
+        name,
+        value: option,
+      },
+      preventDefault: () => { },
+    };
+    onChange(event);
+    setIsOpen(false);
+  };
+
   return (
-    <select
-      name={name}
-      onChange={onChange}
-      required='required'
-      {...(isControlledInput ? { value } : { defaultValue: value })}
-    >
-      <option value="">City</option>
-      {cities.map((city) => (
-        <option
-          key={city.id}
-          value={city.cityName}
-        >
-          {city.cityName}
-        </option>
-      ))}
-    </select>
+    <div className={`App__custom-select ${isOpen ? 'open' : ''}`}>
+      <div className={`App__selected-option ${selectedValue? 'selected' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+        {selectedValue || 'City'}
+      </div>
+      <ul className="App__options">
+        <li onClick={() => handleSelect('')} key="">
+          City
+        </li>
+        {cities.map(({id, cityName}) => (
+          <li
+            key={id}
+            onClick={() => handleSelect(cityName)}
+          >
+            {cityName}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
