89a74856a202f2ef3f6703d0f4ad43828338ee6a
Aina_Kostina <aina.kostina@1clickgames.com>
2023-09-10 18:48:32 +0300

    Added switc toggle titles

--- src/components/SwitchToggler/SwitchToggler.jsx
+++ src/components/SwitchToggler/SwitchToggler.jsx
@@ -1,12 +1,16 @@
 import './SwitchToggler.sass';
 
 const SwitchToggler = ({ isDarkTheme, onChange }) => {
-    return (
-        <div class={`App__switch ${isDarkTheme ? 'active' : ''}`}>
-            <input class="App__switch__check" type="checkbox" onChange={onChange} />
-            <span class="App__switch__toggle"></span>
-        </div>
-    );
+  return (
+    <div className='App__switch-wrapper'>
+      <span className='App__switch-title'>Light</span>
+      <div class={`App__switch ${isDarkTheme ? 'active' : ''}`}>
+        <input class="App__switch-check" type="checkbox" onChange={onChange} />
+        <span class="App__switch-toggle"></span>
+      </div>
+      <span className='App__switch-title'>Dark</span>
+    </div>
+  );
 };
 
 export default SwitchToggler;
