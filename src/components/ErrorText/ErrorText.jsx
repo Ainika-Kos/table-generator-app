b229493848ff998d22fff1832f443c07e1524409
Aina_Kostina <aina.kostina@1clickgames.com>
2023-09-14 01:20:57 +0300

    It is enough for today I want to go to sleep

--- src/components/ErrorText/ErrorText.jsx
+++
@@ -1,9 +0,0 @@
-import './ErrorText.sass';
-
-const ErrorText = ({ errorText }) => {
-    return (
-        <p className='App__form__error-text'>{errorText}</p>
-    );
-};
-
-export default ErrorText;
