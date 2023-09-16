e9b37a758c1be9a8b4e9354425a91c043bdb510d
Aina_Kostina <aina.kostina@1clickgames.com>
2023-09-16 15:38:03 +0300

    Added icons in the toasts

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,9 +1,25 @@
 import './Toast.sass';
+import errorIcon from '../../assets/errorIcon.svg';
+import infoIcon from '../../assets/infoIcon.svg';
+import successIcon from '../../assets/successIcon.svg';
+
 const Toast = ({ toastText, toastStatus }) => {
+
+  const statusIcons = {
+    error: errorIcon,
+    info: infoIcon,
+    success: successIcon
+  }
+
+  const toastIconSrc = statusIcons[toastStatus];
+  const toastStatusText = toastStatus.charAt(0).toUpperCase() + toastStatus.slice(1);
 
   return (
     <div className={`App__toast-container ${toastStatus}`}>
-      <span className='App__toast-title'>{toastStatus}</span>
+      <span className='App__toast-icon'>
+        <img src={toastIconSrc} alt={`${toastStatus} Icon`} />
+      </span>
+      <span className='App__toast-title'>{toastStatusText}</span>
       <p className='App__toast-text'>{toastText}</p>
     </div>
   );
